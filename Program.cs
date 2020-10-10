caa23b239f781815a377ef71c3c8240f38075e42
Rohn07 <Rohan.buddhdev08@gmail.com>
2020-10-10 17:28:55 +0530

    Arrays

--- Program.cs
+++ Program.cs
@@ -6,41 +6,17 @@ namespace CSharpPractice
     {
         static void Main(string[] args)
         {
-           int i = 10; // Assignment operator
-            Console.WriteLine(i);
+            // array declarations and defining the size of the array.
+           int[] Numbers = new int[5];
 
-            int Number1 = 10;
-            int Number2 = 2; 
+            // storing the data for the array
+           Numbers[0] = 1;
+           Numbers[1] = 2;
+           Numbers[2] = 3;
+           Numbers[3] = 4;
+           Numbers[4] = 5;
 
-            // Arthematic operators
-            int Add = Number1 + Number2;
-            int Sub = Number1 - Number2;
-            int Divide = Number1 / Number2;
-            int Remainder = Number1 % Number2;
-            int Multiply = Number1 * Number2;
-
-            Console.WriteLine("Result is {0}", Add);
-            Console.WriteLine("Result is {0}", Sub);
-            Console.WriteLine("Result is {0}", Divide);
-            Console.WriteLine("Result is {0}", Remainder);
-            Console.WriteLine("Result is {0}", Multiply);
-
-            // Comparision operator
-            // "==" and "!=" stands for "equalTo" and "notEqualTo"
-
-            // Conditional Operator
-            // && operator and || operator
-            if (Number1 == 10 && Number2 == 2) {
-                Console.WriteLine("Hello I am AND operator!");
-            } 
-
-            if (Number1 == 10 || Number2 == 21) {
-                Console.WriteLine("Hello I am OR operator!");
-            }
-
-            // Ternary Operator
-            bool isNumber = i == 24 ? true : false;
-            Console.WriteLine(isNumber);
+           Console.WriteLine(Numbers[3]);
         }
     }
 }
