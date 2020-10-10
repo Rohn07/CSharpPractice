38c820250244d74e282591367602f49e950e87f8
Rohn07 <Rohan.buddhdev08@gmail.com>
2020-10-10 18:24:09 +0530

    if/else and switch/case statements

--- Program.cs
+++ Program.cs
@@ -6,17 +6,98 @@ namespace CSharpPractice
     {
         static void Main(string[] args)
         {
-            // array declarations and defining the size of the array.
-           int[] Numbers = new int[5];
+            // IF ELSE STATEMENTS
+            Console.WriteLine("Please enter a number");
+            int Number = int.Parse(Console.ReadLine());
 
-            // storing the data for the array
-           Numbers[0] = 1;
-           Numbers[1] = 2;
-           Numbers[2] = 3;
-           Numbers[3] = 4;
-           Numbers[4] = 5;
+            if(Number == 1) {
+                Console.WriteLine("Your number is one.");
+            }
+            else {
+                Console.WriteLine("Your number is not one");
+            }
 
-           Console.WriteLine(Numbers[3]);
+            // There is a difference between single (|) and double (||).
+            // When an double sign is there it only checks the first condition, 
+            // if there is single sign it checks both the conditions.
+            // Same applies to (&) and double (&&) 
+
+            if (Number == 10 | Number == 20) {
+                Console.WriteLine("Your number is something");
+            }
+
+            // SWITCH STATEMENTS
+            Console.WriteLine("Please enter a number");
+            int NumberSwitch = int.Parse(Console.ReadLine());
+
+            switch(NumberSwitch) 
+            {
+                case 10:
+                {
+                    Console.WriteLine("Your number is 10");
+                    break;
+                }
+                case 20:
+                {
+                    Console.WriteLine("Your number is 20");
+                    break;
+                }
+                case 30:
+                {
+                    Console.WriteLine("Your number is 30");
+                    break;
+                }
+                default:
+                    Console.WriteLine("None");
+                    break;
+            } 
+
+            // SWITCH STATEMENTS WITH GOTO STATEMENTS
+            int totalCoffeeCost = 0;
+
+            Console.WriteLine("Welcome to the coffee station.");
+            Console.WriteLine("Choose the size of the coffee.");
+            start: // labels
+            Console.WriteLine("1 - Small, 2 - Medium, 3 - Large");
+
+            int userChoice = int.Parse(Console.ReadLine());
+
+            switch (userChoice)
+            {
+                case 1:
+                    totalCoffeeCost += 1;
+                    break;
+
+                case 2:
+                    totalCoffeeCost += 2;
+                    break;
+
+                case 3:
+                    totalCoffeeCost += 3;
+                    break;
+
+                default:
+                    Console.WriteLine("Your choice {0} is invalid.", userChoice);
+                    goto start;
+            }
+            decide: // label
+            Console.WriteLine("Do you want another coffee?");
+            Console.WriteLine("Yes OR No");
+            string choice = Console.ReadLine();
+
+            switch (choice.ToUpper()) {
+                case "YES":
+                    goto start;
+                case "NO":
+                    break;
+                default:
+                    Console.WriteLine("Your choice {0} is invalid.", choice);
+                    goto decide;
+            }
+
+            Console.WriteLine("Thankyou for shopping with us");
+            Console.WriteLine("Your bill amount is {0}", totalCoffeeCost);
+            
         }
     }
 }
