e16614163e6d9610131c0d103f8358ce02d6cf59
Rohn07 <Rohan.buddhdev08@gmail.com>
2020-10-10 15:35:59 +0530

    Writing in console

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharpPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first name.");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter last your name.");
+            string lastName = Console.ReadLine();
+
+            // concate method to write on console
+            Console.WriteLine("Hello " +firstName+lastName+ "!");
+            // placeholder method to write on console
+            Console.WriteLine("Welcome {0}, {1}", firstName, lastName);
+        }
+    }
+}
