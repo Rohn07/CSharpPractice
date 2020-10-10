23319662c7ae07390207447df7028336882b2401
Rohn07 <Rohan.buddhdev08@gmail.com>
2020-10-10 19:11:36 +0530

    Types of loops

--- Program.cs
+++ Program.cs
@@ -6,98 +6,56 @@ namespace CSharpPractice
     {
         static void Main(string[] args)
         {
-            // IF ELSE STATEMENTS
-            Console.WriteLine("Please enter a number");
-            int Number = int.Parse(Console.ReadLine());
+            // WHILE LOOP
+            Console.WriteLine("Please enter your target:");
+            int target = int.Parse(Console.ReadLine());
+            int start = 0;
 
-            if(Number == 1) {
-                Console.WriteLine("Your number is one.");
-            }
-            else {
-                Console.WriteLine("Your number is not one");
-            }
-
-            // There is a difference between single (|) and double (||).
-            // When an double sign is there it only checks the first condition, 
-            // if there is single sign it checks both the conditions.
-            // Same applies to (&) and double (&&) 
-
-            if (Number == 10 | Number == 20) {
-                Console.WriteLine("Your number is something");
-            }
-
-            // SWITCH STATEMENTS
-            Console.WriteLine("Please enter a number");
-            int NumberSwitch = int.Parse(Console.ReadLine());
-
-            switch(NumberSwitch) 
+            while (start <= target)
             {
-                case 10:
-                {
-                    Console.WriteLine("Your number is 10");
-                    break;
-                }
-                case 20:
-                {
-                    Console.WriteLine("Your number is 20");
-                    break;
-                }
-                case 30:
-                {
-                    Console.WriteLine("Your number is 30");
-                    break;
-                }
-                default:
-                    Console.WriteLine("None");
-                    break;
-            } 
+                Console.Write(start + " ");
+                start += 2;
+            }
 
-            // SWITCH STATEMENTS WITH GOTO STATEMENTS
-            int totalCoffeeCost = 0;
-
-            Console.WriteLine("Welcome to the coffee station.");
-            Console.WriteLine("Choose the size of the coffee.");
-            start: // labels
-            Console.WriteLine("1 - Small, 2 - Medium, 3 - Large");
-
-            int userChoice = int.Parse(Console.ReadLine());
-
-            switch (userChoice)
+            // DO WHILE LOOP
+            string userChoice = string.Empty;
+            do 
             {
-                case 1:
-                    totalCoffeeCost += 1;
-                    break;
+                Console.WriteLine("Please enter your target:");
+                int target2 = int.Parse(Console.ReadLine());
+                int start2 = 0;
+                
+                while (start2 <= target2)
+                {
+                    Console.Write(start2 + " ");
+                    start2 += 2;
+                }
+                do
+                {
+                    Console.WriteLine("Do you want to continue? Yes or No");
 
-                case 2:
-                    totalCoffeeCost += 2;
-                    break;
+                    userChoice = Console.ReadLine().ToUpper();
+                    if(userChoice != "YES" && userChoice != "NO")
+                    {
+                        Console.WriteLine("Invalid choice.");
+                    }
+                } while (userChoice != "YES" && userChoice != "NO");
+            } while(userChoice == "YES");
 
-                case 3:
-                    totalCoffeeCost += 3;
-                    break;
+            // FOR AND FOR EACH LOOPS
+            int[] numbers = new int[3];
 
-                default:
-                    Console.WriteLine("Your choice {0} is invalid.", userChoice);
-                    goto start;
+            numbers[0] = 101;
+            numbers[1] = 102;
+            numbers[2] = 103;
+
+            for(int i = 0; i <numbers.Length; i++) {
+                Console.WriteLine(numbers[i]);
             }
-            decide: // label
-            Console.WriteLine("Do you want another coffee?");
-            Console.WriteLine("Yes OR No");
-            string choice = Console.ReadLine();
 
-            switch (choice.ToUpper()) {
-                case "YES":
-                    goto start;
-                case "NO":
-                    break;
-                default:
-                    Console.WriteLine("Your choice {0} is invalid.", choice);
-                    goto decide;
+            foreach (int each in numbers) {
+                Console.WriteLine(each);
             }
-
-            Console.WriteLine("Thankyou for shopping with us");
-            Console.WriteLine("Your bill amount is {0}", totalCoffeeCost);
-            
         }
     }
 }
