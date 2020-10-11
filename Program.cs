5f7591237e9e40eb0bad7b6770d0728e89a22153
Rohn07 <Rohan.buddhdev08@gmail.com>
2020-10-12 00:46:44 +0530

    method over loading

--- Program.cs
+++ Program.cs
@@ -1,58 +1,23 @@
 ﻿using System;
-// METHOD OVER RIDING
-// POLYMORPHISM 
+// METHOD OVER LOADING
 namespace CSharpPractice
 {
-    public class Employee
-    {
-        public string firstName = "parent";
-        public string lastame = "class";
-
-        public virtual void printFullName() // adding a virtual keyword would help us to override the parent class method in child class. 
-        {
-            Console.WriteLine(firstName+" "+lastame);
-        }
-    }
-
-    public class partTimeEmployee : Employee
-    {
-        public override void printFullName() // adding the override method 
-        {
-            Console.WriteLine(firstName+" "+lastame+ "- Part time");
-        }
-    }
-
-    public class fullTimeEmployee : Employee
-    {
-        public override void printFullName()
-        {
-            Console.WriteLine(firstName+" "+lastame+ "- Full time");
-        }
-    }
-
-    public class interns : Employee
-    {
-        public override void printFullName()
-        {
-            Console.WriteLine(firstName+" "+lastame+ "- Intern");
-        }
-    }
-
     public class Program
     {
+        public static void Add(int first, int second)
+        {
+            Console.WriteLine("Sum is {0}", first+second);
+        }
+        public static void Add(int first, int second, out int Sum) // same method name, different parameters
+        {
+            Sum = first + second;
+            Console.WriteLine("Sum is {0}", Sum);
+        }
         public static void Main()
         {
-            Employee[] employees = new Employee[4];
-
-            employees[0] = new Employee();
-            employees[1] = new partTimeEmployee();
-            employees[2] = new fullTimeEmployee();
-            employees[3] = new interns();
-
-            foreach (var item in employees)
-            {
-                item.printFullName();
-            }
+            int sum;
+            Add(10,20);
+            Add(20,30,out sum);
         }
     }
 }
