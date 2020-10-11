962372b569d3b93a2458c0270f5f908d519a2008
Rohn07 <Rohan.buddhdev08@gmail.com>
2020-10-11 20:05:40 +0530

    Polymorphism and M_O_Riding

--- Program.cs
+++ Program.cs
@@ -1,13 +1,14 @@
 ﻿using System;
-
+// METHOD OVER RIDING
+// POLYMORPHISM 
 namespace CSharpPractice
 {
     public class Employee
     {
-        public string firstName;
-        public string lastame;
+        public string firstName = "parent";
+        public string lastame = "class";
 
-        public void printFullName()
+        public virtual void printFullName() // adding a virtual keyword would help us to override the parent class method in child class. 
         {
             Console.WriteLine(firstName+" "+lastame);
         }
@@ -15,18 +16,25 @@ namespace CSharpPractice
 
     public class partTimeEmployee : Employee
     {
-        public new void printFullName() // new keyword is required to hide the method in the parent class.
+        public override void printFullName() // adding the override method 
         {
-            Console.WriteLine(firstName+" "+lastame+ "- Contractor");
-            // base.printFullName(); // this method will call the parent class method. first way to call a base class method.
+            Console.WriteLine(firstName+" "+lastame+ "- Part time");
         }
     }
 
     public class fullTimeEmployee : Employee
     {
-        public new void printFullName() // Method hiding with the new keyword
+        public override void printFullName()
         {
-            Console.WriteLine(firstName+" "+lastame+ "- Employee");
+            Console.WriteLine(firstName+" "+lastame+ "- Full time");
+        }
+    }
+
+    public class interns : Employee
+    {
+        public override void printFullName()
+        {
+            Console.WriteLine(firstName+" "+lastame+ "- Intern");
         }
     }
 
@@ -34,16 +42,17 @@ namespace CSharpPractice
     {
         public static void Main()
         {
-            fullTimeEmployee FTE = new fullTimeEmployee(); // creating a refernece for the class
-            FTE.firstName = "Rohan";
-            FTE.lastame = "Buddhdev";
-            FTE.printFullName();
+            Employee[] employees = new Employee[4];
 
-            partTimeEmployee PTE = new partTimeEmployee();
-            PTE.firstName = "Rishabh";
-            PTE.lastame = "Shah";
-            PTE.printFullName();
-            // ((Employee)PTE).printFullName(); // this will call the parent class method // 2nd way to call the parent class employee
+            employees[0] = new Employee();
+            employees[1] = new partTimeEmployee();
+            employees[2] = new fullTimeEmployee();
+            employees[3] = new interns();
+
+            foreach (var item in employees)
+            {
+                item.printFullName();
+            }
         }
     }
 }
